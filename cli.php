1b906a29982503e7360017f7591747c6a8152147
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2018-04-09 18:49:08 +0700

    Fix Core Bug

--- cli.php
+++ cli.php
@@ -62,11 +62,11 @@ if(!file_exists($file1)) {
 }
 $file2=readline("Output Name\t: ");
 $file=file_get_contents($file1);
-if(preg_match("/<?php/",$file)) {
+/*if(preg_match("/<?php/",$file)) {
 	$file=str_replace("<?php","",$file);
-}
+}*/
 $file=encode(base64_encode($file));
-$isi="eval(str_rot13(base64_decode('c2hhcGd2YmEgcXJwYnFyKCRQaW5lMTk4NCl7JGZnZXZhdD0nJztzYmUoJHY9MDskdjxmZ2V5cmEoJFBpbmUxOTg0KS0xOyR2Kz0yKXskZmdldmF0Lj1wdWUodXJrcXJwKCRQaW5lMTk4NFskdl0uJFBpbmUxOTg0WyR2KzFdKSk7fQ0KZXJnaGVhICRmZ2V2YXQ7fSRhdD0nZXJmZic7JG5hPSd0bWgnOyRvdj0nYnFyJzskd3Y9J2FwYnpjJzskbz0nZW5qJzskbj0naGV5cXJwJzskb25vdj0kby4kbi4kb3Y7JG5hd3ZhdD0kbmEuJHd2LiRhdDsg')));eval(\$anjing(\$babi('x%9CS%C9%CE%B7UJJ%2CV%B2%E6R%C9%2B%B1UJ53%89OIM%06q%F3sl%95%F2SRA%CC%EC%FC%BC%12%20%17H%EB%01U%E9%01%A5%AC%01%D7.%10%B0')));eval(\$kontol(decode('".$file."')));";
+$isi="eval(str_rot13(base64_decode('c2hhcGd2YmEgcXJwYnFyKCRQaW5lMTk4NCl7JGZnZXZhdD0nJztzYmUoJHY9MDskdjxmZ2V5cmEoJFBpbmUxOTg0KS0xOyR2Kz0yKXskZmdldmF0Lj1wdWUodXJrcXJwKCRQaW5lMTk4NFskdl0uJFBpbmUxOTg0WyR2KzFdKSk7fQ0KZXJnaGVhICRmZ2V2YXQ7fSRhdD0nZXJmZic7JG5hPSd0bWgnOyRvdj0nYnFyJzskd3Y9J2FwYnpjJzskbz0nZW5qJzskbj0naGV5cXJwJzskb25vdj0kby4kbi4kb3Y7JG5hd3ZhdD0kbmEuJHd2LiRhdDsg')));eval(\$anjing(\$babi('x%9CS%C9%CE%B7UJJ%2CV%B2%E6R%C9%2B%B1UJ53%89OIM%06q%F3sl%95%F2SRA%CC%EC%FC%BC%12%20%17H%EB%01U%E9%01%A5%AC%01%D7.%10%B0')));eval('?>'.\$kontol(decode('".$file."')));";
 $tulis=fopen($file2,"w+");
 fwrite($tulis,$isi);
 fclose($tulis);
